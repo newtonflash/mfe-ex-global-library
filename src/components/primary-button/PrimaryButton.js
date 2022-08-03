aa7d88c1dc39bb0493c1fd42a1e67ff38cbcb79d
Pradyumna Ghose <newton@newtonflash.com>
2022-08-03 12:37:53 +0530

    Add files via upload

---
+++ src/components/primary-button/PrimaryButton.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const PrimaryButton = ({children}) => {
+    return (
+        <button className="primary-button">{children}</button>
+    )
+}
+
+export default PrimaryButton;
